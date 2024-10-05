5987228d821c714300ed8c088a12ff9bf5a39fb3
Sarah Gad <sarahgad298@gmail.com>
2024-10-05 03:56:13 +0300

    created 'registerUserCtrl' to be used to register a new user

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -25,7 +26,7 @@ const UserSchema = new mongoose.Schema({
   profilePhoto: {
     type: Object,
     default: {
-      url: 'https://pixabay.com/vectors/profile-user-internet-man-42914/',
+      url: 'https://cdn.pixabay.com/photo/2012/04/26/19/43/profile-42914_640.png',
       publicId: null,
     },
   },
@@ -45,6 +46,20 @@ const UserSchema = new mongoose.Schema({
 });
 
 const User = mongoose.model('User', UserSchema);
+
+function validateRegisterUser(obj) {
+  const schema = Joi.object({
+    username: Joi.string().trim().min(2).max(100)
+      .required(),
+    email: Joi.string().trim().min(5).max(100)
+      .required()
+      .email(),
+    password: Joi.string().trim().min(8).required(),
+  });
+  return schema.validate(obj);
+}
+
 module.exports = {
   User,
+  validateRegisterUser,
 };
