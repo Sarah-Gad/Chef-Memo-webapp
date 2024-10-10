713d533fbe84c79cae7b4d88ee07059781d88286
Sarah Gad <sarahgad298@gmail.com>
2024-10-10 05:23:33 +0300

    created "createCommentCtrl" controller to add a new comment

---
+++ backend/routes/commentRoute.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { createCommentCtrl } = require('../controllers/commentController');
+const { verifyToken } = require('../middlewares/verifyToken');
+
+router.route('/').post(verifyToken, createCommentCtrl);
+
+module.exports = router;
