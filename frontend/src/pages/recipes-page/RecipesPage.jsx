7f855c74e22bd199d6e67ce46dc855dd873e0b4c
Sarah Gad <sarahgad298@gmail.com>
2024-10-12 15:46:19 +0300

    created Footer component and added it to all pages

--- frontend/src/pages/recipes-page/RecipesPage.jsx
+++ frontend/src/pages/recipes-page/RecipesPage.jsx
@@ -1,11 +1,15 @@
 import "./recipes-page.css";
 import { categories, recipes } from "../../dummyData";
+import { useEffect } from "react";
 import RcipeList from "../../components/recipes/RecipeList";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Pagination from "../../components/pagination/Pagination";
 
 const RecipesPage = () => {
-    
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, []);
+
     return (
     <>
       <section className="recipes-page">
