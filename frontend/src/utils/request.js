1912ddd59b34d2590239b5cd8f0f2859af18f33c
Sarah Gad <sarahgad298@gmail.com>
2024-10-16 09:54:50 +0300

    added the deployed backend base url so that the frontend uses it to send requests to the backend api

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "https://chef-memo-webapp-backend.vercel.app/"
 });
 
 export default request;
