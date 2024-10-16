9c17c36731df5c390cb8bea1d4e7f76eac6e2cff
Sarah Gad <sarahgad298@gmail.com>
2024-10-16 15:29:32 +0300

    implement responsive header (hide login/register text on mobile, show icons only)

--- frontend/src/components/header/HeaderRight.jsx
+++ frontend/src/components/header/HeaderRight.jsx
@@ -43,12 +43,12 @@ const HeaderRight = () => {
             </> : (
                 <>
                 <Link to="/login" className="header-right-link">
-                    <LogOut size={20} />
-                    <span>Login</span>
+                    <LogOut />
+                    <span className="header-right-link-text">Login</span>
                 </Link>
                 <Link to="/register" className="header-right-link">
-                    <UserRoundPlus size={20} />
-                    <span>Register</span>
+                    <UserRoundPlus />
+                    <span className="header-right-link-text">Register</span>
                 </Link>
                 </>
             )
